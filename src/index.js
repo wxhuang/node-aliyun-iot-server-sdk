5c817cb6d76b1438334817f3d1d50bfaff054a14
huangwenxi <wenxi.huang@sen5.com>
2016-09-08 13:42:52 +0800

    add babel info.

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import DeviceGrantAction from './DeviceGrantAction'
+import DevicePermitModifyAction from './DevicePermitModifyAction'
+import DeviceRevokeByIdAction from './DeviceRevokeByIdAction'
+import DeviceRevokeByTopicAction from './DeviceRevokeByTopicAction'
+import ListDevicePermitAction from './ListDevicePermitAction'
+import PubAction from './PubAction'
+import RegistDeviceAction from './RegistDeviceAction'
+import RevertRpcAction from './RevertRpcAction'
+import ServerOnlineAction from './ServerOnlineAction'
+import SubAction from './SubAction'
+import UnSubAction from './UnSubAction'
+import {AcsClient} from './utils'
+
+export {DevicePermitModifyAction,
+  DeviceGrantAction,
+  DeviceRevokeByTopicAction,
+  DeviceRevokeByIdAction,
+  ListDevicePermitAction,
+  PubAction,
+  RevertRpcAction,
+  RegistDeviceAction,
+  ServerOnlineAction,
+  SubAction,
+  UnSubAction,
+  AcsClient,
+};
